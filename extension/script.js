0223cb01ca89dbb3f52fcfbede40fe09f17c764b
Oleg Bakhadze-Karnaukhov <84627505+SmirnovKV-33@users.noreply.github.com>
2023-02-18 20:03:20 +0400

    Big update

--- extension/script.js
+++ extension/script.js
@@ -9,12 +9,14 @@ recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 
 function init(){
-    document.getElementsByTagName('body')[0].innerHTML  = '<div id="av-img-f" style="position: absolute;width: 100%;height: 100%;"><img id="img-shadow" style="position: absolute;bottom: 50px;left: 60px;border-radius: 50%;width: 70px;height: 70px;z-index: 1;" type="image/svg+xml" src="https://cyber-training.ru/public/img_hack.png"></div>' + document.getElementsByTagName('body')[0].innerHTML;
+    document.getElementsByTagName('body')[0].innerHTML  = '<div id="av-img-f" style="position: absolute;width: 100%;height: 100%;"><img id="img-shadow" style="position: fixed;bottom: 50px;left: 60px;border-radius: 50%;width: 70px;height: 70px;z-index: 1;" type="image/svg+xml" src="https://cyber-training.ru/public/img_hack.png"></div>' + document.getElementsByTagName('body')[0].innerHTML;
     var btn = document.getElementById('av-img-f');
     btn.addEventListener('click', function() {
         recognition.start();
+        if(document.getElementById('acd9cfd6-7c02-4fb3-9f0c-f5f5e91414ad') != null){ return; }
         var elem = document.getElementById('acd9cfd6-7c02-4fb3-9f0c-b5f5e91414ad');
         var clone = elem.cloneNode(true);
+        clone.id = 'acd9cfd6-7c02-4fb3-9f0c-f5f5e91414ad';
         clone.childNodes[0].childNodes[0].disabled = true;
         clone.childNodes[0].style = "border-color: #0035ff; box-shadow: 0 8px 8px #ebeffa;"
         const rec = document.createElement("div");
@@ -51,11 +53,11 @@ recognition.onspeechend = function() {
 }
 
 recognition.onstart = function() {
-    document.getElementById('img-shadow').style = 'box-shadow: 0px 1px 20px 1px #8b65d8; position: absolute;bottom: 50px;left: 60px;border-radius: 50%;width: 70px;height: 70px;z-index: 1;';
+    document.getElementById('img-shadow').style = 'box-shadow: 0px 1px 20px 1px #8b65d8; position: fixed;bottom: 50px;left: 60px;border-radius: 50%;width: 70px;height: 70px;z-index: 1;';
 }
 
 recognition.onend = function() {
-    document.getElementById('img-shadow').style = 'position: absolute;bottom: 50px;left: 60px;border-radius: 50%;width: 70px;height: 70px;z-index: 1;';
+    document.getElementById('img-shadow').style = 'position: fixed;bottom: 50px;left: 60px;border-radius: 50%;width: 70px;height: 70px;z-index: 1;';
 }
 
 recognition.onnomatch = function(event) {
