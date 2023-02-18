a96bb60a3a5ba03450d61b069ecc2b3ab8c76743
Oleg Bakhadze-Karnaukhov <84627505+SmirnovKV-33@users.noreply.github.com>
2023-02-19 02:25:41 +0400

    Update

--- extension/script.js
+++ extension/script.js
@@ -8,6 +8,19 @@ recognition.lang = 'ru-RU';
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 
+function show_assistant(){
+    document.getElementsByTagName('article')[0].childNodes[1].style = 'display: none;';
+    document.getElementsByTagName('article')[0].innerHTML = document.getElementsByTagName('article')[0].innerHTML + '<div class="app-article__content app-article_content_3MJ8N app-article_paddedContent_2xRP2" style=" min-height: 800px; "><div class="project-edit-aside_wrapper_3nc9n"><h3 class="project-edit-aside_title_1WGHZ">ЦИФРОВОЙ ПОМОЩНИК - КСЮША</h3><div class="project-edit-aside_text_3CJjF"><p>Цифровой ассистент поможет тебе легко найти, все что связано с твоей грантовой заявкой! Тебе достаточно лишь надиктовать (или напечатать) твой вопрос. Она подберет нужные курсы, уроки, статьи!</p><p>Если тебе нужна помощь с заполнением какого-то грантового поля, ты тоже можешь попросить ее помочь. Просто скажи «Ксюша, помоги заполнить поле цель проекта. Цель моего проекта обучить людей киберграмотности» и так можно делать с любым полем!</p></div></div><form class="project-edit-form_form_1RCXW"><!----><section class="base-row"><h3 class="project-edit-form_title_3J8qN">Твой вопрос</h3><div class="base-textarea base-field base-field--animated base-field--grid base-field--grid-12" bound="region" parts="" rules="[object Object]" required="required"><div class="base-field__entry"><textarea id="question-field" name="acd9cfd6-7c02-4fb3-9f0c-b5f5e91414ad" rows="2" tabindex="0" class="base-textarea__autosize" required="required" style="resize: none; height: 90px; overflow: hidden;"></textarea><!----><!----></div><label for="acd9cfd6-7c02-4fb3-9f0c-b5f5e91414ad-102" id="question-label" title="С чем тебе необходима помощь?" class="base-field__label base-field__label--empty"><span>С чем тебе необходима помощь?</span><div icon="asterisk" path="/icons/asterisk.svg" class="base-icon base-field__marker"><svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" focusable="false"><path d="M11 6h2v4.079l3.341-2.34 1.147 1.639L13.743 12l3.745 2.622-1.147 1.639L13 13.92V18h-2v-4.079l-3.341 2.34-1.148-1.639L10.257 12 6.51 9.378l1.15-1.639L11 10.08V6z" fill="currentColor"></path></svg></div></label><!----><button id="btn-filling" style="margin-top: 20px; display:none;" type="button" aria-label="" tabindex="0" custom="" class="base-button base-button--alternate"><span class="base-button__label base-button__label-icon-prefix base-button__label-text"><span class="base-button__text"> Перенести в заявку <!----></span><!----></span></button></div><div id="answer-card" style="display: none;"><h3 class="project-edit-form_title_3J8qN">Ответ на вопрос</h3><div class="project-edit-form_title_3J8qN" id="ai-answer"></div></div></section></form></div>';
+    // Обработка события аналогового воода текста (с клавы)
+    var keyboard = document.getElementById('question-field');
+    keyboard.addEventListener('change', function() {
+        rewrite_interface_update(document.getElementById('question-field').value);
+    });
+}
+function hide_assistant(){
+    document.getElementsByTagName('article')[0].childNodes[1].style = 'display: block;';
+}
+
 // Получение данных рерайтинга с бэкенда
 async function call_rewrite(text){
     let response = await fetch("http://127.0.0.1:5000/", {
@@ -30,57 +43,35 @@ async function call_rewrite(text){
 
 // Метод обновления интерфейса при запуске рерайтинга
 async function rewrite_interface_update(text){
+    document.getElementById('btn-filling').style = 'margin-top: 20px; display: none;';
     // Двигаем лейбл первого поля
-    var elem = document.getElementById('acd9cfd6-7c02-4fb3-9f0c-b5f5e91414ad').childNodes[1];
-    elem.classList.remove('base-field__label--empty');
-    elem.classList.add('base-field__label', 'base-field__label--focused');
-    document.getElementById('acd9cfd6-7c02-4fb3-9f0c-b5f5e91414ad').childNodes[0].childNodes[0].value = text;
+    var label = document.getElementById('question-label');
+    label.classList.remove('base-field__label--empty');
+    label.classList.add('base-field__label', 'base-field__label--focused');
+    document.getElementById('question-field').value = text;
     const recommendation = await call_rewrite(text);
-    // Двигаем лейбл второго поля
-    var elem = document.getElementById('acd9cfd6-7c02-4fb3-9f0c-f5f5e91414ad').childNodes[1];
-    elem.classList.remove('base-field__label--empty');
-    elem.classList.add('base-field__label', 'base-field__label--focused');
-    document.getElementById('acd9cfd6-7c02-4fb3-9f0c-f5f5e91414ad').childNodes[0].childNodes[0].value = recommendation.result;
+    document.getElementById('answer-card').style = 'display: block;';
+    document.getElementById('ai-answer').innerText = recommendation.result;
+    if(recommendation.field_id != false){
+        document.getElementById('btn-filling').style = 'margin-top: 20px; display: block;';
+        var btn = document.getElementById('btn-filling');
+        btn.addEventListener('click', function() {
+            hide_assistant();
+            document.getElementById(recommendation.field_id).value = recommendation.result;
+            let updated_label = document.querySelectorAll("[for='" + recommendation.field_id + "']")[0];
+            updated_label.classList.remove('base-field__label--empty');
+            updated_label.classList.add('base-field__label', 'base-field__label--focused');
+        });
+    }
 }
 
 function init(){
+    
     document.getElementsByTagName('body')[0].innerHTML  = "<div id='av-img-f' style='position: absolute;width: 100%;height: 100%;'><div style='position: fixed; bottom: 50px; left: 91px; border-radius: 8px; width: 200px; height: 70px; z-index: 6; border: 2px solid #8b65d8; background: #0000000f;'><img id='img-shadow' style='position: fixed;bottom: 50px;left: 60px;border-radius: 50%;width: 70px;height: 70px;z-index: 1;' type='image/svg+xml' src='https://cyber-training.ru/public/img_hack.png'><div style=' height: calc(70px - 6px); display: flex; flex-direction: column; justify-content: center; padding-left: 52px; '><div style='font-weight: 700;'>Ксюша</div><div>Твой ассистент</div></div></div></div>" + document.getElementsByTagName('body')[0].innerHTML;
     // Обработка события «записи микрофона»
     var btn = document.getElementById('av-img-f');
     btn.addEventListener('click', function() {
         recognition.start();
-        if(document.getElementById('acd9cfd6-7c02-4fb3-9f0c-f5f5e91414ad') != null){ return; }
-        var elem = document.getElementById('acd9cfd6-7c02-4fb3-9f0c-b5f5e91414ad');
-        var clone = elem.cloneNode(true);
-        clone.id = 'acd9cfd6-7c02-4fb3-9f0c-f5f5e91414ad';
-        clone.childNodes[0].childNodes[0].disabled = true;
-        clone.childNodes[0].style = "border-color: #0035ff; box-shadow: 0 8px 8px #ebeffa;"
-        const rec = document.createElement("div");
-        rec.classList.add('base-field__description');
-        rec.textContent = 'Это Ксюша, я перефразировала это поле';
-        clone.appendChild(rec);
-        elem.after(clone);
-        document.getElementById('acd9cfd6-7c02-4fb3-9f0c-f5f5e91414ad').childNodes[1].innerText = 'Рекомендация по изменению краткого описания проекта'
-        document.getElementById('acd9cfd6-7c02-4fb3-9f0c-f5f5e91414ad').childNodes[0].childNodes[0].value = 'ИИ в процессе форматирования и переписывания текста, пожалуйста подождите... ';
-    });
-
-    // Обработка события аналогового воода текста (с клавы)
-    var keyboard = document.getElementById('acd9cfd6-7c02-4fb3-9f0c-b5f5e91414ad');
-    keyboard.addEventListener('change', function() {
-        var elem = document.getElementById('acd9cfd6-7c02-4fb3-9f0c-b5f5e91414ad');
-        rewrite_interface_update(elem.childNodes[0].childNodes[0].value);
-        if(document.getElementById('acd9cfd6-7c02-4fb3-9f0c-f5f5e91414ad') != null){ return; }
-        var clone = elem.cloneNode(true);
-        clone.id = 'acd9cfd6-7c02-4fb3-9f0c-f5f5e91414ad';
-        clone.childNodes[0].childNodes[0].disabled = true;
-        clone.childNodes[0].style = "border-color: #0035ff; box-shadow: 0 8px 8px #ebeffa;"
-        const rec = document.createElement("div");
-        rec.classList.add('base-field__description');
-        rec.textContent = 'Это Ксюша, я перефразировала это поле';
-        clone.appendChild(rec);
-        elem.after(clone);
-        document.getElementById('acd9cfd6-7c02-4fb3-9f0c-f5f5e91414ad').childNodes[1].innerText = 'Рекомендация по изменению краткого описания проекта'
-        document.getElementById('acd9cfd6-7c02-4fb3-9f0c-f5f5e91414ad').childNodes[0].childNodes[0].value = 'ИИ в процессе форматирования и переписывания текста, пожалуйста подождите... ';
     });
 }
 
@@ -108,6 +99,7 @@ recognition.onspeechend = function() {
 }
 
 recognition.onstart = function() {
+    show_assistant();
     document.getElementById('img-shadow').style = 'box-shadow: 0px 1px 20px 1px #8b65d8; position: fixed;bottom: 50px;left: 60px;border-radius: 50%;width: 70px;height: 70px;z-index: 1;';
 }
 
--- model/server.py
+++ model/server.py
@@ -5,15 +5,30 @@ import import_ipynb
 import model
 import json
 
-
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/", methods = ['POST'])
 def collector():
     field = request.json.get('field')
-    value = { 
-        "result": model.text_rewrite(field) 
-    }
-
-    return json.dumps(value)
+    # Импортируем JSON 
+    dump = open('patterns.json')
+    patterns = json.load(dump)
+    for pattern in patterns:
+        if (field.find(pattern["substring"]) != -1) and (pattern["field_id"] == False):
+            return json.dumps(pattern)
+    
+    # Если рерайтинг запускаем нейронку
+    for pattern in patterns:
+        if (field.find(pattern["substring"]) != -1):
+            return json.dumps({ 
+                "result": model.text_rewrite(field),
+                "field_id": pattern["field_id"]  
+            })
+            
+    # Дефолтный возврат
+    return json.dumps({ 
+        "result": "Друг, у меня нет ответов на твой вопрос! Но скоро он появится!)",
+        "field_id": False  
+    })
+    
