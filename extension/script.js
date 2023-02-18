dc51a7597464a428144f2ce3dbf4ae3db96a28ce
Oleg Bakhadze-Karnaukhov <84627505+SmirnovKV-33@users.noreply.github.com>
2023-02-18 23:33:20 +0400

    Design refactoring

--- extension/script.js
+++ extension/script.js
@@ -8,6 +8,7 @@ recognition.lang = 'ru-RU';
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 
+// Получение данных рерайтинга с бэкенда
 async function call_rewrite(text){
     let response = await fetch("http://127.0.0.1:5000/", {
         method: "POST",
@@ -27,8 +28,24 @@ async function call_rewrite(text){
     }
 }
 
+// Метод обновления интерфейса при запуске рерайтинга
+async function rewrite_interface_update(text){
+    // Двигаем лейбл первого поля
+    var elem = document.getElementById('acd9cfd6-7c02-4fb3-9f0c-b5f5e91414ad').childNodes[1];
+    elem.classList.remove('base-field__label--empty');
+    elem.classList.add('base-field__label', 'base-field__label--focused');
+    document.getElementById('acd9cfd6-7c02-4fb3-9f0c-b5f5e91414ad').childNodes[0].childNodes[0].value = text;
+    const recommendation = await call_rewrite(text);
+    // Двигаем лейбл второго поля
+    var elem = document.getElementById('acd9cfd6-7c02-4fb3-9f0c-f5f5e91414ad').childNodes[1];
+    elem.classList.remove('base-field__label--empty');
+    elem.classList.add('base-field__label', 'base-field__label--focused');
+    document.getElementById('acd9cfd6-7c02-4fb3-9f0c-f5f5e91414ad').childNodes[0].childNodes[0].value = recommendation.result;
+}
+
 function init(){
-    document.getElementsByTagName('body')[0].innerHTML  = '<div id="av-img-f" style="position: absolute;width: 100%;height: 100%;"><img id="img-shadow" style="position: fixed;bottom: 50px;left: 60px;border-radius: 50%;width: 70px;height: 70px;z-index: 1;" type="image/svg+xml" src="https://cyber-training.ru/public/img_hack.png"></div>' + document.getElementsByTagName('body')[0].innerHTML;
+    document.getElementsByTagName('body')[0].innerHTML  = "<div id='av-img-f' style='position: absolute;width: 100%;height: 100%;'><div style='position: fixed; bottom: 50px; left: 91px; border-radius: 8px; width: 200px; height: 70px; z-index: 6; border: 2px solid #8b65d8; background: #0000000f;'><img id='img-shadow' style='position: fixed;bottom: 50px;left: 60px;border-radius: 50%;width: 70px;height: 70px;z-index: 1;' type='image/svg+xml' src='https://cyber-training.ru/public/img_hack.png'><div style=' height: calc(70px - 6px); display: flex; flex-direction: column; justify-content: center; padding-left: 52px; '><div style='font-weight: 700;'>Ксюша</div><div>Твой ассистент</div></div></div></div>" + document.getElementsByTagName('body')[0].innerHTML;
+    // Обработка события «записи микрофона»
     var btn = document.getElementById('av-img-f');
     btn.addEventListener('click', function() {
         recognition.start();
@@ -43,10 +60,31 @@ function init(){
         rec.textContent = 'Это Ксюша, я перефразировала это поле';
         clone.appendChild(rec);
         elem.after(clone);
+        document.getElementById('acd9cfd6-7c02-4fb3-9f0c-f5f5e91414ad').childNodes[1].innerText = 'Рекомендация по изменению краткого описания проекта'
+        document.getElementById('acd9cfd6-7c02-4fb3-9f0c-f5f5e91414ad').childNodes[0].childNodes[0].value = 'ИИ в процессе форматирования и переписывания текста, пожалуйста подождите... ';
+    });
+
+    // Обработка события аналогового воода текста (с клавы)
+    var keyboard = document.getElementById('acd9cfd6-7c02-4fb3-9f0c-b5f5e91414ad');
+    keyboard.addEventListener('change', function() {
+        var elem = document.getElementById('acd9cfd6-7c02-4fb3-9f0c-b5f5e91414ad');
+        rewrite_interface_update(elem.childNodes[0].childNodes[0].value);
+        if(document.getElementById('acd9cfd6-7c02-4fb3-9f0c-f5f5e91414ad') != null){ return; }
+        var clone = elem.cloneNode(true);
+        clone.id = 'acd9cfd6-7c02-4fb3-9f0c-f5f5e91414ad';
+        clone.childNodes[0].childNodes[0].disabled = true;
+        clone.childNodes[0].style = "border-color: #0035ff; box-shadow: 0 8px 8px #ebeffa;"
+        const rec = document.createElement("div");
+        rec.classList.add('base-field__description');
+        rec.textContent = 'Это Ксюша, я перефразировала это поле';
+        clone.appendChild(rec);
+        elem.after(clone);
+        document.getElementById('acd9cfd6-7c02-4fb3-9f0c-f5f5e91414ad').childNodes[1].innerText = 'Рекомендация по изменению краткого описания проекта'
+        document.getElementById('acd9cfd6-7c02-4fb3-9f0c-f5f5e91414ad').childNodes[0].childNodes[0].value = 'ИИ в процессе форматирования и переписывания текста, пожалуйста подождите... ';
     });
 }
 
-recognition.onresult = async function(event) {
+recognition.onresult = function(event) {
     // Create the interim transcript string locally because we don't want it to persist like final transcript
     let interim_transcript = "";
     let final_transcript = "";
@@ -60,18 +98,9 @@ recognition.onresult = async function(event) {
         interim_transcript += event.results[i][0].transcript;
       }
     }
-    // Двигаем лейбл первого поля
-    var elem = document.getElementById('acd9cfd6-7c02-4fb3-9f0c-b5f5e91414ad').childNodes[1];
-    elem.classList.remove('base-field__label--empty');
-    elem.classList.add('base-field__label', 'base-field__label--focused');
+    // Текстовый запрос
     const text = (final_transcript == '' ? interim_transcript : final_transcript);
-    document.getElementById('acd9cfd6-7c02-4fb3-9f0c-b5f5e91414ad').childNodes[0].childNodes[0].value = text;
-    const recommendation = await call_rewrite(text);
-    // Двигаем лейбл второго поля
-    var elem = document.getElementById('acd9cfd6-7c02-4fb3-9f0c-f5f5e91414ad').childNodes[1];
-    elem.classList.remove('base-field__label--empty');
-    elem.classList.add('base-field__label', 'base-field__label--focused');
-    document.getElementById('acd9cfd6-7c02-4fb3-9f0c-f5f5e91414ad').childNodes[0].childNodes[0].value = recommendation.result;
+    rewrite_interface_update(text);
 }
   
 recognition.onspeechend = function() {
@@ -87,11 +116,11 @@ recognition.onend = function() {
 }
 
 recognition.onnomatch = function(event) {
-    diagnostic.textContent = "I didn't recognise that color.";
+    diagnostic.textContent = "Не опознанная ошибка.";
 }
   
 recognition.onerror = function(event) {
-    diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
+    diagnostic.textContent = 'Не опознанная ошибка: ' + event.error;
 }
   
-setTimeout(() => init(), 5000);
+setTimeout(() => init(), 3000);
